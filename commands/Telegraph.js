b2f6d7ad5dac043bd1aff54db61c796011b88197
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-12-20 14:22:22 +0500

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -23,7 +23,7 @@ cmd({
     },
     async(Void, citel,text) => {
  let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-2.0+Bot&author_name=Slasher-Official &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=HASI-MD+Bot&author_name=Naveeddogar &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
     }
 );
