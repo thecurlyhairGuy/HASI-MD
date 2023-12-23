0843c6c9c3c801c6f535fcab40f38a15719f72e9
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-12-23 13:43:34 +0500

    HASI-MD BOT V1

--- commands/misc.js
+++ commands/misc.js
@@ -1,5 +1,5 @@
 /**
-//---------------------------------------------------------------------------
+---------------------------------------------------------------------------
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ╔══╗░░░╔══╗░╔═════════╗░╔═════════╗░╔══╗   ╔══╗   ╔══╗ ╔═════╗                  
 ║██║░░░║██║░║█████████║░║█████████║░║██║░░░███╗░░░███╗██████╗░ 
@@ -10,7 +10,16 @@
 ╚══╝░░░╚══╝░╚══╝░░░╚══╝░╚════════╝░ ╚══╝░░░╚═╝░░░░░╚═╝╚═════╝░
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░  
 --------------------------------------------------------------------------
-**/
+ Copyright (C) 2022.                                                                                        
+ Licensed under the  GPL-3.0 License;                                                      
+ You may not use this file except in compliance with the License.    
+ It is supplied in the hope that it may be useful                                     
+ * @project_name : HASI-MD                                                                   
+ * @author : chhaseeb47 <https://github.com/chhaseeb47>   
+ * @description : HASI-Md ,A Multi-functional whatsapp bot.       
+ * @version 1.0.1                                                                                             
+ ========================================================
+ **/
 
  const { tlang, getAdmin, prefix, Config, sck, fetchJson, runtime,cmd,getBuffer } = require('../lib')
  let { dBinary, eBinary } = require("../lib/binary");
