d5e1dd1791a65c87a32be35205fd4a99b1291685
CH HASEEB <mhmodsyt517@gmail.com>
2023-12-27 20:16:00 -0800

    HASI-MD BOT V1

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
 
-
+ 
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://chhaseeb517:chhaseeb517@cluster0.1dtvf4q.mongodb.net/?retryWrites=true&w=majority"
